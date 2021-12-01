e91a1e67b1e2f7783285a1946c5d27f012c2e991
praph <praphweb@gmail.com>
2021-12-01 17:34:57 +0200

    dark mode sample ready

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-console.log(rxjs);
-
 const { Observable, from } = rxjs;
+let darkMode = false
+
+const btnOn = document.getElementById('dark-mode-on')
+const btnOff = document.getElementById('dark-mode-off')
+const darkModeDiv = document.getElementById('dark-mode')
+
+btnOn.addEventListener('click', () => {
+  darkMode = true
+})
+btnOff.addEventListener('click', () => {
+  darkMode = false
+})
+
+darkModeDiv.innerHTML = darkMode
